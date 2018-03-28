af3f806dc4a0e9a67d9a736f39d34b87585f9788
vsgirelli <vavi_sg@hotmail.com>
2018-03-27 22:59:12 -0300

    Final gosta2 and something about gosta3

--- catp/5catp/gosta2.pl
+++ catp/5catp/gosta2.pl
@@ -11,17 +11,25 @@ gosta(paulo,X) :- gosta(maria,X).   /* Paulo gosta de tudo que Maria gosta */
 gosta(pedro,X) :- gosta(X,boliche). /* pedro gosta de quem gosta de boliche*/
 gosta(pedro,boliche).         /* Pedro gosta de boliche */
 gosta(pedro,X) :- gosta(X,X). /* Pedro gosta de qualquer um que goste de si mesmo */
-
 /*
- * continua exibindo o que pedro gosta (leitura e cinema)
- * continua exibindo que pedro gosta de quem gosta de boliche (paulo, maria e vera).
- * como é definido antes que paulo gosta de tudo que maria gosta,
- * então é afirmado que paulo gosta de boliche, então pedro gosta de paulo.
- * como depois é definido que pedro gosta de boliche, então vale que pedro gosta dele mesmo
- * (porque pedro gosta de quem gosta de boliche)
- * então, como pedro gosta de quem gosta de si mesmo,
- * então pedro gosta de si mesmo.... assim entra em loop.
- * Pelo que eu entendi, a cada vez que a regra é aplicada, ele cria uma nova "verdade" no BDI
- * então ele avalia essa verdade logo em seguida.
- * Então, quando ele avalia que ele gosta de quem gosta de si mesmo,
+ * O motor de inferência faz retrocessos até a última escolha feita, portanto,
+ * exibe as respostas normais do que pedro gosta (leitura e cinema),
+ * e quando encontra gosta(paulo, X) :- gosta(maria, X), faz uma nova árvore pra gosta(maria, X).
+ * Não haverá nenhum resultado para essa árvore, pois nada do que paulo gosta é importante no momento.
+ * Quando chega em gosta(pedro, X) :- gosta(X, boliche), é feita uma nova árvore para gosta(X, boliche).
+ * Nessa árvore, são encontradas as respostas de quem gosta de boliche (paulo, maria e vera),
+ * e quando chega em gosta(paulo, X) :- gosta(maria, X), faz novamente uma nova árvore pra gosta(maria, X).
+ * Dessa vez, um resultado é importante, que é o resutado que diz que paulo gosta de boliche pois
+ * maria também gosta de boliche. Essa resposta é importante quando retorna para a árvore
+ * gosta(pedro, X) :- gosta(X, boliche), pois o que se busca ali é que pedro vai gostar de quem
+ * gostar de boliche, e sendo que paulo vai gostar do que maria gosta, paulo gosta de boliche, então
+ * pedro gosta de paulo.
+ * Ainda na árvore gosta(pedro, X) :- gosta(X, boliche), indo para a próxima alternativa,
+ * é visto que pedro gosta de boliche também, então pedro gosta de pedro.
+ * Depois que sai da árvore acima, vai para gosta(pedro, boliche), então boliche é uma resposta para a pergunta inicial.
+ * Como todas as respostas válidas até aquele ponto foram encontradas, o motor de inferência retrocede
+ * e vai para a próxima alternativa.
+ * Quando chega em gosta(pedro,X) :- gosta(X,X), faz novamente uma árvora, mas entra em loop pois
+ * verifica que pedro gosta de pedro (porque pedro gosta de boliche, e pedro gosta de todo mundo que gosta de boliche),
+ * e vai encontrar novamente em gosta(pedro, X) :- gosta(X, X), criando nova árvore e etc...
  */
---
+++ catp/5catp/gosta3.pl
@@ -0,0 +1,14 @@
+gosta(pedro,leitura).
+gosta(maria,leitura).
+gosta(paulo,leitura).
+gosta(pedro,cinema).
+gosta(paulo,cinema).
+gosta(vera,cinema).
+gosta(paulo,boliche).
+gosta(maria,boliche).
+gosta(vera,boliche).
+gosta(paulo,X) :- gosta(maria,X).   /* Paulo gosta de tudo que Maria gosta */
+gosta(pedro,X) :- gosta(X,boliche). /* pedro gosta de quem gosta de boliche*/
+gosta(pedro,boliche).         /* Pedro gosta de boliche */
+gosta(pedro,X) :- gosta(X,X). /* Pedro gosta de qualquer um que goste de si mesmo */
+combina(X,Y,Z) :- gosta(X,Z), gosta(Y,Z). /* Existe algo (Z) que X e Y gostam? */
